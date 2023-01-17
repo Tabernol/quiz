c4e88bfaa08d15839f00f16ea857e570a358e738
MaxKrasnoloskyi <makskrasnopolskyi@gmail.com>
2023-01-17 21:43:24 +0200

    21

--- src/main/java/validator/DataValidator.java
+++ src/main/java/validator/DataValidator.java
@@ -6,10 +6,10 @@ public class DataValidator {
     private static final String REGEX_LOGIN = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
             + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
     private static final String REGEX_PASSWORD = "^.{4,10}$";
-    private static final String REGEX_NAME = "^[a-zA-ZА-Яа-я *]{2,20}$";
-    private static final String REGEX_NAME_NUMBER = "^[0-9a-zA-ZА-Яа-я *]{2,20}$";
+    private static final String REGEX_NAME = "^[a-zA-Zа-щА-ЩЬьЮюЯяЇїІіЄєҐґ *]{2,30}$";
+    private static final String REGEX_NAME_NUMBER = "^[0-9a-zA-Zа-щА-ЩЬьЮюЯяЇїІіЄєҐґ *]{2,30}$";
 
-    private static final String REGEX_NOT_LONG_STRING = "^[\\s\\S]{1,20}$";
+    private static final String REGEX_NOT_LONG_STRING = "^[\\s\\S]{1,30}$";
     private static final Integer MIN_DIFFICULT = 1;
     private static final Integer MAX_DIFFICULT = 100;
     private static final Integer MIN_DURATION = 1;
